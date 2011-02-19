91d11fd7f9bf911c6abc59a18e17ec5890cee148
John Hodge <tpg@mutabah.net>
2011-02-19 16:07:50 +0800

    Door config settings (and some fixes to doorgroup reporting)

--- src/server/handler_door.c
+++ src/server/handler_door.c
@@ -14,6 +14,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define	DEBUG	1
+
 #define DOOR_UNLOCKED_DELAY	10	// 10 seconds before it re-locks
 
 // === IMPORTS ===
@@ -30,16 +32,12 @@ tHandler	gDoor_Handler = {
 	Door_CanDispense,
 	Door_DoDispense
 };
-char	*gsDoor_Password;
-char	*gsDoor_Command;
+char	*gsDoor_Password = "";
 // int	giDoor_SerialFD;
 
 // == CODE ===
 int Door_InitHandler(void)
-{		
-	gsDoor_Command = malloc(sizeof("llogin door -w ")+strlen(gsDoor_Password));
-	sprintf(gsDoor_Command, "llogin door -w %s", gsDoor_Password);
-	
+{	
 	return 0;
 }
 
@@ -47,11 +45,23 @@ int Door_InitHandler(void)
  */
 int Door_CanDispense(int User, int Item)
 {
+	#if DEBUG
+	printf("Door_CanDispense: (User=%i,Item=%i)\n", User, Item);
+	#endif
 	// Sanity please
-	if( Item == 0 )	return -1;
+	if( Item != 0 )	return -1;
 	
 	if( !(Bank_GetFlags(User) & USER_FLAG_DOORGROUP) )
+	{
+		#if DEBUG
+		printf("Door_CanDispense: User %i not in door\n", User);
+		#endif
 		return 1;
+	}
+	
+	#if DEBUG
+	printf("Door_CanDispense: User %i can open the door\n", User);
+	#endif
 	
 	return 0;
 }
@@ -62,25 +72,55 @@ int Door_CanDispense(int User, int Item)
 int Door_DoDispense(int User, int Item)
 {
 	FILE	*pipe;
+	char	buf[512];	// Buffer flush location - the sewer :)
+	
+	#if DEBUG
+	printf("Door_DoDispense: (User=%i,Item=%i)\n", User, Item);
+	#endif
+	
 	// Sanity please
 	if( Item != 0 )	return -1;
 	
 	// Check if user is in door
 	if( !(Bank_GetFlags(User) & USER_FLAG_DOORGROUP) )
+	{
+		#if DEBUG
+		printf("Door_CanDispense: User %i not in door\n", User);
+		#endif
 		return 1;
+	}
 	
 	// llogin or other
-	//pipe = popen(gsDoor_Command, "w");
 	pipe = popen("llogin door -w -", "w");
-	if( !pipe || pipe == (void*)-1 )
+	if( !pipe || pipe == (void*)-1 ) {
+		#if DEBUG
+		printf("Door_DoDispense: llogin failure\n");
+		#endif
 		return -1;
+	}
+	if( fread(buf, 512, 1, pipe) == 0 )	return -1;	// Flush!
 	
-	fputs(gsDoor_Password, pipe);	fputs("\n", pipe);
+	// Send password
+	fputs(gsDoor_Password, pipe);
+	fputs("\n", pipe);
+	if( fread(buf, 512, 1, pipe) == 0 )	return -1;	// Flush!
+	
+	// ATH1 - Unlock door
 	fputs("ATH1\n", pipe);
+	if( fread(buf, 512, 1, pipe) == 0 )	return -1;	// Flush!
+	
+	// Wait before re-locking
 	sleep(DOOR_UNLOCKED_DELAY);
+
+	// Re-lock the door
 	fputs("ATH0\n", pipe);
+	if( fread(buf, 512, 1, pipe) == 0 )	return -1;	// Flush!
 	
 	pclose(pipe);
+	
+	#if DEBUG
+	printf("Door_DoDispense: User %i opened door\n", User);
+	#endif
 
 	return 0;
 }
