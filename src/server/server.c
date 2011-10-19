aa61d80edb7d282468861c0d2f72a729715e7786
John Hodge <tpg@ucc.asn.au>
2011-10-19 17:30:36 +0800

    Server - Added bac to mark zero priced slots as erroring

--- src/server/server.c
+++ src/server/server.c
@@ -713,6 +713,8 @@ void Server_int_SendItem(tClient *Client, tItem *Item)
 		}
 	}
 	
+	if( Item->Price == 0 )
+		status = "error";
 	// KNOWN HACK: Naming a slot 'dead' disables it
 	if( strcmp(Item->Name, "dead") == 0 )
 		status = "sold";	// Another status?
